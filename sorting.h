5056d1412b2e3bf4e9ca08794d737ac3e6d26c0c
pennuengkas <pennueng.kas@student.mahidol.edu>
2026-01-28 08:52:09 +0000

    selective sort

--- sorting.h
+++ sorting.h
@@ -1,6 +1,7 @@
 void swap(int &f,int &s);
 void sort(int *a,int n);
 void display(int *a,int n);
+void selecionsort(int *a, int n);
 #include <iomanip>
 
 
@@ -17,17 +18,44 @@ void display(int *a, int n)
 void sort(int *a, int n)
 {
     int i,j;
+    int sorted;
     //bubble sort - compare 7 element 6 times to get all pair so n elements compare n-1 times
     for (j=0;j<n-1;j++)
     {
+        sorted = 0;
         for(i = 0; i<n-1-j; i++)
         {
             /*for ascending order*/
-            if(a[i]>a[i+1]) swap(a[i],a[i+1]);
+            if(a[i]>a[i+1]) 
+            {
+                swap(a[i],a[i+1]);
+                sorted=1;
+            }
         }
+        display(a,n);
+        if(sorted==0) break;
+    }
+}
+
+void selectionsort(int *a, int n)
+{
+    for (int j=0;j<n;j++)
+    {
+        int i, mi;
+        int min=a[j];
+        mi=j;    
+        for (i=1+j;i<n;i++)
+        {
+            if (min>a[i]){
+                min=a[i];
+                mi=i;
+            }
+        }
+        swap(a[j], a[mi]);
         display(a,n);
     }
 }
+
 
 void swap(int &f, int &s)
 {
